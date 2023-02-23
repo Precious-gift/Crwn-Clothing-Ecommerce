d5f5da3499c303110a60b602e830b8cf2ebeadf8
Precious-gift <falayeprecious@gmail.com>
2023-02-23 05:32:31 +0100

    updating our directory page with directory component and category item component

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import categoriesList from "./components/categories-List/categories-list";
+import Categories from "./components/categories/categories.component";
+
+const App = () => {
+  return <Categories list={categoriesList} />;
+};
+
+export default App;
---
+++ src/components/categories-List/categories-list.js
@@ -0,0 +1,34 @@
+const categoriesList = [
+  {
+    id: 1,
+    title: "Hats",
+    imageUrl:
+      "https://images.unsplash.com/photo-1529958030586-3aae4ca485ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aGF0c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 2,
+    title: "Jackets",
+    imageUrl:
+      "https://images.unsplash.com/photo-1551488831-00ddcb6c6bd3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8amFja2V0c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 3,
+    title: "Sneakers",
+    imageUrl:
+      "https://images.unsplash.com/photo-1565814636199-ae8133055c1c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c25lYWtlcnN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 4,
+    title: "Womens",
+    imageUrl:
+      "https://images.unsplash.com/photo-1615233500570-c5d7576b4262?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8d29tZW5zfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 5,
+    title: "Mens",
+    imageUrl:
+      "https://images.unsplash.com/photo-1552374196-1ab2a1c593e8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8bWVuc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+  },
+];
+
+export default categoriesList;
